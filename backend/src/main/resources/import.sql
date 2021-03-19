b1543d18117493fd548a5029f1a68dac96f774c9
isacfarias <isac.farias.21@gmail.com>
2021-03-18 21:23:10 -0400

    Product entity, SQL script

--- backend/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-insert into tb_category (name, created_at) values ('Eletrônicos', NOW());
-insert into tb_category (name, created_at) values ('Books', NOW());
-insert into tb_category (name, created_at) values ('Computs', NOW());
