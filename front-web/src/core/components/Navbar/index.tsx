b88b7c97d1c4078bfd1d70f396db4d7854cb876f
isacfarias <isac.farias.21@gmail.com>
2021-03-27 23:18:50 -0400

    customização scss

---
+++ front-web/src/core/components/Navbar/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.scss';
+
+const Navbar = () => (
+    <nav className="row bg-primary main-nav">
+
+        <div className="col-2">
+            <a href="#" className="nav-logo-text">
+                <h4>Ds Catalog</h4>
+            </a>
+        </div>
+
+    </nav>
+);
+
+export default Navbar;
--- front-web/src/index.tsx
+++ front-web/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+
+import App from './App';
 
 
 ReactDOM.render(
