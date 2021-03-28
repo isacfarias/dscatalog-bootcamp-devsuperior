1867f58cba2fb96873a5af5197e2b53bf3ccdf39
isacfarias <isac.farias.21@gmail.com>
2021-03-27 23:50:53 -0400

    add styles for navbar

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -9,6 +9,23 @@ const Navbar = () => (
                 <h4>Ds Catalog</h4>
             </a>
         </div>
+        <div className="col-6 offset-2">
+
+            <ul className="main-menu">
+                <li>
+                    <a href="#" className="active">HOME</a>
+                </li>
+
+                <li>
+                    <a href="#">CATÁLOGO</a>
+                </li>
+
+                <li>
+                    <a href="#">ADMIN</a>
+                </li>
+            </ul>
+
+        </div>
 
     </nav>
 );
