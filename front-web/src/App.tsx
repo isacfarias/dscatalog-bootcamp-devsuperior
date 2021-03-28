c37e90df955957417ca1adb747b41fd95f57b1ff
isacfarias <isac.farias.21@gmail.com>
2021-03-27 21:50:10 -0400

    clean project

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,30 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React  from 'react';
 
 const App = () => {
-
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    console.log("componente inicializado", counter);
-  }, [counter]);
-
   return (
     <div className="container mt-5">
-      <button type="button" className="btn btn-primary mr-5"
-      onClick={() => setCounter(counter + 1)}
-      >
-        +
-      </button>
-      <span>
-      {counter}
-      </span>
-      <button type="button" className="btn btn-primary ml-5"
-      onClick={() => setCounter(counter - 1)}
-      >
-        -
-      </button>
-
-      { counter > 5 &&  <h1> Valor é maior que 5</h1> }
+   
     </div>
   );
 }
