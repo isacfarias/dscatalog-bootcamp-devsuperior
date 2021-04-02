a356e0d1ed2c5ffe69b6bea65d2120fce60355d9
isacfarias <isac.farias.21@gmail.com>
2021-04-02 11:17:18 -0400

    implementação de detalhes de produto

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import { ReactComponent as ArrowIcon } from '../../../../core/assets/images/arrow.svg';
+import { ReactComponent as ProductImage } from '../../../../core/assets/images/product.svg';
 import './styles.scss';
 
 
@@ -9,7 +10,7 @@ type ParamsType = {
     productId: string;
 }
 
-const ProductDetails = () =>  {
+const ProductDetails = () => {
 
     const { productId } = useParams<ParamsType>();
 
@@ -20,6 +21,28 @@ const ProductDetails = () =>  {
                     <ArrowIcon className="icon-goback" />
                     <h1 className="text-goback">Voltar</h1>
                 </Link>
+                <div className="row">
+
+                    <div className="col-6 pr-5">
+                        <div className="product-details-card border-radios-20 text-center">
+                            <ProductImage className="product-details-image" />
+                        </div>
+                        <h1 className="product-datails-name">
+                            Computador Desktop - Intel Core i7
+                        </h1>
+                        <span className="product-currency">R$</span>
+                        <h3 className="product-price">2779,00</h3>
+                    </div>
+
+                    <div className="col-6 product-details-card border-radios-20">
+                        <h1 className="product-description-title">Descrição do Produto</h1>
+                        <p className="product-description-text">
+                            Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.
+                        </p>
+                    </div>
+
+                </div>
+
             </div>
         </div>
     );
