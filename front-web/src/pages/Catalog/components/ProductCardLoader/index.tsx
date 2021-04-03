5779081e66932a31590bc56224753785cb6b14b9
isacfarias <isac.farias.21@gmail.com>
2021-04-02 22:14:40 -0400

    add content loader catalago

---
+++ front-web/src/pages/Catalog/components/ProductCardLoader/index.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const ProductCardLoader = () => {
+  const loadItens = [0, 1, 2];
+  return (
+    <>
+      {
+        loadItens.map(item =>
+          <ContentLoader
+            key={item}
+            speed={1}
+            width={250}
+            height={335}
+            viewBox="0 0 250 335"
+            backgroundColor="#f1f2f2"
+            foregroundColor="#E1E1E1"
+          >
+            <rect x="0" y="0" rx="10" ry="10" width="250" height="335" />
+          </ContentLoader>
+        )}
+    </>
+  );
+}
+
+export default ProductCardLoader
+
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -3,23 +3,29 @@ import { Link } from 'react-router-dom';
 import { ProductResponse } from '../../core/types/Product';
 import { makeRequest } from '../../core/utils/request';
 import ProductCard from './components/ProductCard';
+import ProductCardLoader from './components/ProductCardLoader';
 import './styles.scss';
 
 const Catalog = () => {
     // quando o componente iniciar o componente deve buscar a lista
     // quando a lista de produtos estiver disponivel,
-     const [productResponse, setProductResponse] = useState<ProductResponse>();
-    
+    const [productResponse, setProductResponse] = useState<ProductResponse>();
+    const [isLoading, setisLoading] = useState(false);
+
     // popular um estado no componente, e listar os produtos dinamicamente
     useEffect(() => {
 
         const params = {
             page: 0,
-            linesPerPage:5
+            linesPerPage: 5
         };
-            
-        makeRequest({ url:'/produtcs/', params })
-        .then(response => setProductResponse(response.data));
+
+        setisLoading(true);
+        makeRequest({ url: '/produtcs/', params })
+            .then(response => setProductResponse(response.data))
+            .finally(() => {
+                setisLoading(false);
+            });
 
     }, []);
 
@@ -33,11 +39,14 @@ const Catalog = () => {
         </h1>
 
             <div className="catalog-products">
-                {
+
+                { isLoading ? <ProductCardLoader /> : (
                     productResponse?.content.map(product => (
-                        <Link to={`/products/${product.id}`} key={product.id} ><ProductCard product={product} /></Link>
+                        <Link to={`/products/${product.id}`} key={product.id} >
+                            <ProductCard product={product} />
+                        </Link>
                     ))
-                }
+                )}
             </div>
 
         </div>
