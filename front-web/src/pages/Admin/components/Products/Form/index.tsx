dbe54fbbfef870642f08bac3d17e7f3478ac1ca5
isacfarias <isac.farias.21@gmail.com>
2021-04-04 21:21:09 -0400

    add estrutura padrão para criar formularios

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,18 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
 
+type FormState = {
+    name: string;
+    category: string;
+    price: string;
+}
+
+
 const Form = () => {
 
-    return (
-        
-        <BaseForm title="Cadastrar Produto">
-            <div className="row">
-                <div className="col-6"></div>
-                <div className="col-6"></div>
-            </div>
+    const [formData, setFormData] = useState<FormState>({
+        name:  '',
+        category: '',
+        price: ''
+    });
 
-        </BaseForm>
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const name = event.target.name;
+        const value = event.target.value; 
+        setFormData(data => ({ ...data, [name]: value}));
+        console.log({name, value});
+
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(formData);
+      
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <BaseForm title="Cadastrar Produto">
+                <div className="row">
+                    <div className="col-6">
+                        
+                        <input type="text"
+                            name="name"
+                            className="form-control  mb-5  my-5"
+                            onChange={handleOnChange}
+                            value={formData.name}
+                        />
+
+                        <select value={formData.category}
+                           className="form-control mb-5" 
+                           onChange={handleOnChange}
+                           name="category">
+                            <option value="livro">Livros</option>
+                            <option value="eletronicos">Eletronicos</option>
+                            <option value="computadores">Computadores</option>
+                        </select>
+
+                         <input type="text"
+                            name="price"
+                            className="form-control  mb-5"
+                            onChange={handleOnChange}
+                            value={formData.price}
+                        />
+                    </div>
+                    <div className="col-6"></div>
+                </div>
+
+            </BaseForm>
+        </form>
     );
 
 }
