eb9bf167444db5a55c6226738ae84968a2196d64
isacfarias <isac.farias.21@gmail.com>
2021-04-21 21:01:47 -0400

    implements s3 bucket

--- backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -1,13 +1,25 @@
 package com.devsuperior.dscatalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DscatalogApplication {
+import com.devsuperior.dscatalog.services.S3Service;
 
+@SpringBootApplication
+public class DscatalogApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		service.uploadFile("/home/farias/Imagens/ds_rc_001.png");
 	}
 
 }
